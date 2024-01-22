3a794b15b92583556ae9617075461eb977e0559c
Sobanim <sobolev.dmirtiy@gmail.com>
2024-01-22 22:45:51 +0100

    Prepare FE for BE

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,9 +1,47 @@
 import React, {FC} from 'react';
 
-const FormField: FC = () => {
+
+interface FormFieldPropsI {
+    labelName: string;
+    type: React.ButtonHTMLAttributes<HTMLButtonElement>;
+    name: string;
+    value: string;
+    placeholder: string;
+    handleChange: (e: HTMLBodyElementEventMap) => void;
+    isSurpriseMe: boolean;
+    handleSurpriseMe: () => void;
+
+}
+const FormField: FC<FormFieldPropsI> = ({labelName, type, name, value, handleChange, placeholder, isSurpriseMe, handleSurpriseMe}) => {
     return (
         <div>
-            FormField
+            <div className='flex items-center gap-2 mb-2'>
+                <label
+                    htmlFor={name}
+                    className='block text-sm font-medium text-gray-900'
+                >
+                    {labelName}
+                    {isSurpriseMe && (
+                        <button
+                        type={"button"}
+                        onClick={handleSurpriseMe}
+                        className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+                        >
+                            Surprise me
+                        </button>
+                    )}
+                </label>
+            </div>
+            <input
+                type={type}
+                id={name}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+                required
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+            />
         </div>
     );
 };
